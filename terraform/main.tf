d2828f29b62f3c324d7eab334586947c9a5b44db
root <root@ip-172-31-4-5.ap-south-1.compute.internal>
2025-06-24 10:46:01 +0000

    node js app to ecs deploy

---
+++ terraform/main.tf
@@ -0,0 +1,111 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "${var.app_name}-vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.region}a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.app_name}-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "${var.region}b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.app_name}-subnet-2"
+  }
+}
+
+resource "aws_ecr_repository" "repo" {
+  name = var.app_name
+}
+
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "${var.app_name}-task-exec-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = { Service = "ecs-tasks.amazonaws.com" },
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_policy_attach" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_security_group" "ecs_sg" {
+  name   = "${var.app_name}-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = "${var.app_name}-cluster"
+}
+
+resource "aws_ecs_task_definition" "task" {
+  family                   = "${var.app_name}-task"
+  cpu                      = "256"
+  memory                   = "512"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = var.app_name,
+      image     = "${aws_ecr_repository.repo.repository_url}:latest",
+      essential = true,
+      portMappings = [{ containerPort = 3000 }]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "service" {
+  name            = "${var.app_name}-svc"
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+    security_groups  = [aws_security_group.ecs_sg.id]
+    assign_public_ip = true
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,2 @@
+variable "app_name" { default = "my-node-app" }
+variable "region"   { default = "us-east-1" }
